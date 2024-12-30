c633cd93eff2bbab10455f31aab38dbc8d74c984
tqkblx <il.mystafa@gmail.com>
2024-12-30 07:54:49 +0300

    Complete the attacked program

--- main.c
+++ main.c
@@ -2,25 +2,36 @@
 #include <stdlib.h>
 
 typedef struct {
-  char string[32];
+  char string[4];
   int *pointer;
 } Data;
 
 int main() {
   Data data;
+  int integer;
 
-  printf("Reading 1 string and 1 integer\n");
+  data.pointer = &integer;
 
-  printf("Character pointer: %p\n", data.string);
-  printf("Integer pointer: %p\n", data.pointer);
+  printf("Reading 1 integer and 1 string\n");
+  printf("Pointer value before: %p\n",
+         data.pointer);
 
-  printf("Enter string and integer separated by space (\"yuiop 4\"): ");
-
-  scanf(" %s", data.string);
+  printf("Enter integer: ");
   scanf(" %d", data.pointer);
+  printf("Integer value: %d\n", *data.pointer);
 
-  printf("String: %s\n", data.string);
-  printf("Integer: %d\n", *data.pointer);
+  printf("Enter string: ");
+  scanf(" %s", data.string);
+
+  printf("Pointer value after (smashed): %p\n", data.pointer);
+
+  printf("<");
+  printf("Some ");
+  printf("other ");
+  printf("code ");
+  printf("here");
+  printf(">");
+  printf("\n");
 
   return 0;
 }
